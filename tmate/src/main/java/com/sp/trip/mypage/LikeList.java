2e204411f13a52092f7ffe43c4a5c35b93ab66a1
aj <aj@DESKTOP-2OSLSHL>
2022-06-19 22:40:30 +0900

    0619-찜목록 최아정

--- tmate/src/main/java/com/sp/trip/mypage/LikeList.java
+++ tmate/src/main/java/com/sp/trip/mypage/LikeList.java
@@ -2,35 +2,17 @@ package com.sp.trip.mypage;
 
 public class LikeList {
 // 사진, 숙소이름, 평점, 가격, 지역
-	private int rphotoName;
+	private String rphotoName;
 	private String lodgName;
 	private int revGrade;
 	private int roomWd_price; // 평일 요금
 	private int roomSeasonprice; // 성수기 요금
 	private String mhAddr1; 
 	private String mhaddr;
-	
-	public String getMhaddr() {
-		return mhaddr;
-	}
-	public void setMhaddr(String mhaddr) {
-		this.mhaddr = mhaddr;
-	}
-	private String mhId;
-	private int roomNum;
-	private String roomName;
-	
-	
-	public String getRoomName() {
-		return roomName;
-	}
-	public void setRoomName(String roomName) {
-		this.roomName = roomName;
-	}
-	public int getRphotoName() {
+	public String getRphotoName() {
 		return rphotoName;
 	}
-	public void setRphotoName(int rphotoName) {
+	public void setRphotoName(String rphotoName) {
 		this.rphotoName = rphotoName;
 	}
 	public String getLodgName() {
@@ -63,18 +45,13 @@ public class LikeList {
 	public void setMhAddr1(String mhAddr1) {
 		this.mhAddr1 = mhAddr1;
 	}
-	public String getMhId() {
-		return mhId;
+	public String getMhaddr() {
+		return mhaddr;
 	}
-	public void setMhId(String mhId) {
-		this.mhId = mhId;
+	public void setMhaddr(String mhaddr) {
+		this.mhaddr = mhaddr;
 	}
-	public int getRoomNum() {
-		return roomNum;
-	}
-	public void setRoomNum(int roomNum) {
-		this.roomNum = roomNum;
-	}
+	
 	
 	
 	
